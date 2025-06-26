8868f76db98df03847398cf58c679c2e89ff75bf
RifkySetyaRahman <rifkyalahmed@gmail.com>
2025-06-27 01:51:32 +0800

    psrf-4 std

--- app/Http/Controllers/Proxy/ProxyController.php
+++ app/Http/Controllers/Proxy/ProxyController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Proxy;
 
 use Illuminate\Http\Request;
 use App\Services\HttpProxyService;
+use App\Http\Controllers\Controller;
 
 class ProxyController extends Controller
 {
